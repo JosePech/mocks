89f07e11f255902019409c9215a6af22398f5497
Jose Pech <jose.pech@thoughtworks.com>
2024-11-19 23:24:48 +0100

    dont use mocks

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,6 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.mockito:mockito-core:5.14.2")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
     testImplementation(kotlin("test"))
 }
 
--- src/test/kotlin/domain/services/BookSearchTest.kt
+++ src/test/kotlin/domain/services/BookSearchTest.kt
@@ -4,23 +4,17 @@ import org.example.domain.services.Availability
 import org.example.domain.services.BookSearch
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.any
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.whenever
 
 class BookSearchTest {
-    private lateinit var availability: Availability
     private lateinit var bookSearch: BookSearch
 
     @BeforeEach
     fun setUp() {
-        availability = mock()
-        bookSearch = BookSearch(availability)
+        bookSearch = BookSearch(Availability())
     }
 
     @Test
     fun shouldReturnBookGivenASearchTitle() {
-        whenever(availability.getStock(any())).thenReturn(10)
         val books = bookSearch.findBooksByTitle("The Hobbit")
         assert(books.size == 1)
         assert(books[0].title == "The Hobbit")
@@ -28,14 +22,12 @@ class BookSearchTest {
 
     @Test
     fun shouldReturnEmptyLisIfNoBooksMatchTheSearchTitle() {
-        whenever(availability.getStock(any())).thenReturn(10)
         val books = bookSearch.findBooksByTitle("Unknown Title")
         assert(books.isEmpty())
     }
 
     @Test
     fun shouldNotReturnBooksWithoutStock() {
-        whenever(availability.getStock(any())).thenReturn(0)
         val books = bookSearch.findBooksByTitle("The Silmarillion")
         assert(books.isEmpty())
     }
